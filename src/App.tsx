e6003a3e108f78f90ff52a3361c25878af37ccc0
Andrew <thum14252@gmail.com>
2024-03-17 08:26:00 +0630

    need to fix alot

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
-import { Drawer } from "@mui/material";
-import LinearProgress from "@mui/material";
-import Grid from "@mui/material";
-import Badge from "@mui/material";
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
 
 
 import { Wrapper, StyledButton } from "./App.style";
@@ -68,7 +72,27 @@ const App = () => {
   if (isLoading) return <Laoder />;
   if (error) return <div>Something Went Wrong....</div>
   return (
-    <div></div>
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   )
 }
 
---
+++ src/Cart/Cart.tsx
@@ -0,0 +1,33 @@
+import { Wrapper } from "../App.style";
+import { CardItemType } from "../App";
+import CartItem from "../CartItem/CartItem";
+
+
+type Props = {
+    cartItems: CardItemType[];
+    addToCart: (clickedItem: CardItemType) => void;
+    removeFromCart: (id: number) => void;
+}
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+    const calculateTotal = (item: CardItemType[]) => {
+        item.reduce((ack: number, item: { amount: number; price: number; }) => ack + item.amount * item.price, 0);
+
+        return (
+            <Wrapper>
+                <h2>Your Shopping Cart</h2>
+                {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+                {cartItems.map(item => (
+                    <CartItem
+                        key={item.id}
+                        item={item}
+                        addToCart={addToCart}
+                        removeFromCart={removeFromCart}
+                    />
+                ))}
+                <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+            </Wrapper>
+        )
+    }
+}
+export default Cart;
